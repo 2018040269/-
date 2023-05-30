456bc60fc14dd29774c22af5d32e10d2230dc81f
ZJZ <m18986212061@163.com>
2023-05-30 09:15:23 +0800

    使用select以及default完成原材料的判断 在Produce函数里，用厨师特征进行循环遍历

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ const (
 
 type Cooker struct {
 	name       string
-	totalNum   int           //需完成的总量
+	totalNumP  int           //需完成的总量
 	presentNum int           //目前完成的份数
 	perTime    time.Duration //做一个寿司需要的时间
 }
@@ -35,27 +35,37 @@ var count = 0
 var (
 	logFilename = flag.String("log", "diary.log", "helpmessage")
 )
+var Total = make(chan int, totalNum)
 
 func (cook *Cooker) Produce(in chan<- string) {
-	for count = 0; count < totalNum; count++ {
-		time.Sleep(cook.perTime)
-		cook.presentNum++
-		s1 := cook.name
-		s2 := strconv.FormatInt(int64(cook.presentNum), 10)
-		s3 := s1 + "做的第" + s2 + "个寿司"
-		in <- s3
 
-		fmt.Println(cook.name+"制作了", s3)
-		log.Println(cook.name+"制作了", s3)
+	for i := 0; i < cook.totalNumP; i++ {
+		select {
+		case v := <-Total:
+			fmt.Printf("第%v份材料\n", v)
+			log.Printf("第%v份材料\n", v)
+
+			cook.presentNum++
+			s1 := cook.name
+			s2 := strconv.FormatInt(int64(cook.presentNum), 10)
+			s3 := s1 + "做的第" + s2 + "个寿司"
+			in <- s3
+			fmt.Println(cook.name+"制作了寿司", s3)
+			log.Println(cook.name+"制作了寿司", s3)
+
+		default:
+			fmt.Println("原材料用完了。")
+		}
 	}
+
 }
 
 func (eat *Customer) Buy(out <-chan string) {
 	for i := 0; i < eat.buyNum; i++ {
 		j := <-out
-		time.Sleep(eat.perEatTime)
-		fmt.Println(eat.name+"吃了", j)
-		log.Println(eat.name+"吃了", j)
+		//time.Sleep(eat.perEatTime)
+		fmt.Println(eat.name+"吃了寿司", j)
+		log.Println(eat.name+"吃了寿司", j)
 	}
 }
 
@@ -63,7 +73,7 @@ func diary() {
 	log.Printf("\n")
 	log.Printf("\n")
 	log.Printf("工作完成，闭店\n")
-	log.Printf("已做寿司：%v，传送带现有寿司数：%v\n", count, len(Ch))
+	//log.Printf("已做寿司：%v，传送带现有寿司数：%v\n", count, len(Ch))
 	log.Printf("\n")
 	log.Printf("\n")
 }
@@ -82,6 +92,10 @@ func main() {
 
 	defer diary()
 
+	for i := 1; i <= 100; i++ {
+		Total <- i
+	}
+
 	cookers := [m]Cooker{
 		{"厨师A", 50, 0, 2e9},
 		{"厨师B", 30, 0, 3e9},
@@ -89,15 +103,15 @@ func main() {
 	}
 
 	customers := [n]Customer{
-		{"顾客01", 5, 3e9},
-		{"顾客02", 3, 4e9},
-		{"顾客03", 6, 3e9},
-		{"顾客04", 2, 2e9},
-		{"顾客05", 1, 6e9},
-		{"顾客06", 7, 2e9},
-		{"顾客07", 4, 2e9},
-		{"顾客08", 1, 4e9},
-		{"顾客09", 5, 3e9},
+		{"顾客1", 5, 3e9},
+		{"顾客2", 3, 4e9},
+		{"顾客3", 6, 3e9},
+		{"顾客4", 2, 2e9},
+		{"顾客5", 1, 6e9},
+		{"顾客6", 7, 2e9},
+		{"顾客7", 4, 2e9},
+		{"顾客8", 1, 4e9},
+		{"顾客9", 5, 3e9},
 		{"顾客10", 2, 3e9},
 		{"顾客11", 4, 6e9},
 		{"顾客12", 3, 4e9},
